abf6e35b40ad383de98bfe940dadaa9cb40d0dcb
me0giazodoi <110475358+me0giazodoi@users.noreply.github.com>
2023-04-21 15:58:32 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T2207A_APC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
